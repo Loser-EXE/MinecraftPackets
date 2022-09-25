656fb30d9e6b044efe77813391ce82b17190ed62
LoserEXE <mr.moomoocower167@gmail.com>
2022-09-25 12:49:55 -0500

    Added: ServerList Write To Json

--- src/main/java/com/loserexe/adapters/DescriptionAdapter.java
+++ src/main/java/com/loserexe/adapters/DescriptionAdapter.java
@@ -5,8 +5,10 @@ import com.google.gson.TypeAdapter;
 import com.google.gson.stream.JsonReader;
 import com.google.gson.stream.JsonWriter;
 import com.loserexe.pojo.info.Description;
+import com.loserexe.pojo.text.Extra;
 
 import java.io.IOException;
+import java.util.List;
 
 public class DescriptionAdapter extends TypeAdapter<Description> {
     private final Gson gson;
@@ -17,7 +19,17 @@ public class DescriptionAdapter extends TypeAdapter<Description> {
 
     @Override
     public void write(JsonWriter jsonWriter, Description description) throws IOException {
-        throw new RuntimeException("Not Implemented");
+        jsonWriter.beginObject();
+        jsonWriter.name("extra");
+        jsonWriter.beginArray();
+        List<Extra> extra = description.getExtra();
+        for (int i = 0; i < extra.size(); i++) {
+            writeExtra(jsonWriter, extra.get(i));
+        }
+        jsonWriter.endArray();
+        jsonWriter.name("text");
+        jsonWriter.value(description.getText());
+        jsonWriter.endObject();
     }
 
     @Override
@@ -32,5 +44,40 @@ public class DescriptionAdapter extends TypeAdapter<Description> {
             default:
                 throw new RuntimeException("Expected object or string, not " + jsonReader.peek());
         }
+    }
+
+    private void writeExtra(JsonWriter jsonWriter, Extra extra) throws IOException {
+        jsonWriter.beginObject();
+
+        if (extra.isBold()) {
+            jsonWriter.name("bold");
+            jsonWriter.value(true);
+        }
+
+        if (extra.isStrikeThrough()) {
+            jsonWriter.name("strikeThrough");
+            jsonWriter.value(true);
+        }
+
+        if (extra.getColor() != null) {
+            jsonWriter.name("color");
+            jsonWriter.value(extra.getColor());
+        }
+
+        if (extra.getExtra() != null) {
+            jsonWriter.name("extra");
+            jsonWriter.beginArray();
+            for (int i = 0; i < extra.getExtra().size(); i++) {
+                writeExtra(jsonWriter, extra.getExtra().get(i));
+            }
+            jsonWriter.endArray();
+        }
+
+        if (extra.getText() != null) {
+            jsonWriter.name("text");
+            jsonWriter.value(extra.getText());
+        }
+
+        jsonWriter.endObject();
     }
 }
